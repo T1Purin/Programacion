a3c330db065bb00ba9739dc2998e9b1c210f2c44
Cinn4moroll <sebastian141718floresflores@gmail.com>
2024-10-22 20:07:24 -0300

    Add files via upload

---
+++ Tarea.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import matplotlib.pyplot as plt 
+import streamlit as st
+
+st.header('Mi primera aplicación de streamlit')
+st.subheader('¡Hola,Streamlit!')
+st.write('Esta es una aplicación simple')
+st.image('images.png')
+st.button('Dale click')
+st.text_input('Escriba algo')
