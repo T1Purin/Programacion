13dbb7ec62f12cf8840200cce51050bcb927f793
K4m1K4z3 <sebastian141718floresflores@gmail.com>
2024-10-23 07:23:57 -0300

    Actualizar PrimerTrabajo.py

--- PrimerTrabajo.py
+++ PrimerTrabajo.py
@@ -10,6 +10,7 @@ st.image('images.png')
 st.button('Haz click')
 tx = st.text_input('Escriba algo:')
 st.write(f'Escribiste: {tx}')
+st.link_button("Go", "https://www.google.com/search?q=cinnamoroll&client=ms-android-samsung-ss&sca_esv=60483cdc6baef8bd&udm=2&biw=360&bih=649&sxsrf=ADLYWIIbZOViADMRSsxF8vUfelksTkyyXA%3A1729678930051&ei=Us4YZ_fnArX75OUPkvnEqAU&oq=cinnamo&gs_lp=EhJtb2JpbGUtZ3dzLXdpei1pbWciB2Npbm5hbW8qAggDMgQQIxgnMgQQIxgnMgQQIxgnMg0QABiABBixAxhDGIoFMg0QABiABBixAxhDGIoFSPIRUOEFWIMHcAB4AJABAJgBV6AB6QGqAQEzuAEByAEA-AEBmAIDoAL-AcICCBAAGIAEGLEDwgIKEAAYgAQYQxiKBZgDAIgGAZIHATOgB4ET&sclient=mobile-gws-wiz-img")
 
 #Barra lateral
 st.sidebar.header('Mi primera barra lateral de streamlit')
