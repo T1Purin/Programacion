12e65ab3ce6fedcd822551e6f85675aa203ecb15
K4m1K4z3 <sebastian141718floresflores@gmail.com>
2024-10-23 07:00:29 -0300

    Actualizar PrimerTrabajo.py

--- PrimerTrabajo.py
+++ PrimerTrabajo.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import streamlit as st
 
 #Cuerpo
-st.header('Mi primera aplicación de       streamlit')
+st.header('Mi primera aplicación de streamlit')
 st.subheader('¡Hola,Streamlit!')
 st.write('Esta es una aplicación simple')
 st.image('images.png')
