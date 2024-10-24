20dd3471cb1f5469acf418c66a88b93a495148c6
K4m1K4z3 <sebastian141718floresflores@gmail.com>
2024-10-23 22:54:29 -0300

    Update ejemplo_titanic.py

--- Codigos/ejemplo_titanic.py
+++ Codigos/ejemplo_titanic.py
@@ -3,7 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # Carga el archivo CSV "database_titanic.csv" en un DataFrame de pandas.
-df = pd.read_csv("https://github.com/T1Purin/Programacion_2/blob/main/Archivos/database_titanic.csv")
+df = pd.read_csv("https://github.com/T1Purin/Programacion_2/blob/main/Archivos/database_titanic.csv", sep=';')
 
 # Muestra un título y una descripción en la aplicación Streamlit.
 st.write("""
