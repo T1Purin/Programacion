11dbe59e5431d8be2e0e0381a083e930a8bd049a
K4m1K4z3 <sebastian141718floresflores@gmail.com>
2024-10-25 07:14:48 -0300

    Actualizar Amooor.py

--- Codigos/Amooor.py
+++ Codigos/Amooor.py
@@ -1,4 +1,4 @@
 import streamlit as st
 
 st.head('Hace click :D')
-st.image('https://www.google.com/search?client=ms-android-samsung-ss&sca_esv=9cb25f21911ea42e&sxsrf=ADLYWIJSPoBS5x1qGREsT_tEVRRC6BjOfA:1729851039851&q=cinnamoroll+gif&udm=2&fbs=AEQNm0CbCVgAZ5mWEJDg6aoPVcBgWizR0-0aFOH11Sb5tlNhdwiPJWZFf-MdNGF5cE7ER6ksc8uP1I2s59dA09Tzf42V3bm5F_k0tThvuvLKliumZKzukURrA8TzYyd0ZDEMIxygTfuwcsoM-ATREmlcQ8C82YTXrj3CDVTH1l0Z32MIoXueVCdc21NaO5EFSbZq8ofjgEBYr_Fv8D8LIS7gBbfFaY-pUA&sa=X&ved=2ahUKEwidn_qlpamJAxXPlJUCHc69Kn4QtKgLegQIFRAB&biw=360&bih=649&dpr=3#vhid=UGrELOcfbFYeDM&vssid=mosaic')
+st.link_button('Aquiii!!!','https://www.google.com/search?client=ms-android-samsung-ss&sca_esv=9cb25f21911ea42e&sxsrf=ADLYWIJSPoBS5x1qGREsT_tEVRRC6BjOfA:1729851039851&q=cinnamoroll+gif&udm=2&fbs=AEQNm0CbCVgAZ5mWEJDg6aoPVcBgWizR0-0aFOH11Sb5tlNhdwiPJWZFf-MdNGF5cE7ER6ksc8uP1I2s59dA09Tzf42V3bm5F_k0tThvuvLKliumZKzukURrA8TzYyd0ZDEMIxygTfuwcsoM-ATREmlcQ8C82YTXrj3CDVTH1l0Z32MIoXueVCdc21NaO5EFSbZq8ofjgEBYr_Fv8D8LIS7gBbfFaY-pUA&sa=X&ved=2ahUKEwidn_qlpamJAxXPlJUCHc69Kn4QtKgLegQIFRAB&biw=360&bih=649&dpr=3#vhid=UGrELOcfbFYeDM&vssid=mosaic')
