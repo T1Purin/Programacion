06f72fb4ccefbd0d38892b0251c89413be0b460f
K4m1K4z3 <sebastian141718floresflores@gmail.com>
2024-10-27 20:13:44 -0300

    Update app.py

--- Codigos/app.py
+++ Codigos/app.py
@@ -4,4 +4,4 @@ import streamlit as st
 d1 = pd.read_csv('Archivos/AlarmasSistema.csv', sep=';')
 st.write('Lectura de archivos')
 st.write(d1.head())
-
+st.image('Archivos/0-0-jojos-bizarre-adventure-glosario-diccionario-terminos.webp')
