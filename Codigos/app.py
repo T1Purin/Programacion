f1252d345ea0b3f224810aa47f1dcbeb256bf006
K4m1K4z3 <sebastian141718floresflores@gmail.com>
2024-10-27 11:17:44 -0300

    Update app.py

--- Codigos/app.py
+++ Codigos/app.py
@@ -1,5 +1,5 @@
 import pandas as pd
 
-d1 = pd.read_csv('/home/Cinnamoroll/Documentos/Archivos/Python/Github/Programacion/Archivos/AlarmasSistema.csv', sep=';')
+d1 = pd.read_csv('Archivos/AlarmasSistema.csv', sep=';')
 print(d1.head())
 
