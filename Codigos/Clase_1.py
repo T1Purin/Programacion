768c52930233b3b0b7a45e7d34e57e2857379f50
K4m1K4z3 <sebastian141718floresflores@gmail.com>
2024-10-29 15:37:00 -0300

    Update Clase_1.py

--- Codigos/Clase_1.py
+++ Codigos/Clase_1.py
@@ -11,12 +11,3 @@ def run():
   st.button('Haz click')
   tx = st.text_input('Escriba algo:')
   st.write(f'Escribiste: {tx}')
-  
-  #Barra lateral
-  st.sidebar.header('Mi primera barra lateral de streamlit')
-  st.sidebar.subheader('¡Hola,Barra Lateral!')
-  st.sidebar.write('Esto es una barra lateral')
-  st.sidebar.image('Archivos/images.png')
-  st.sidebar.button('Haz click pero en la barra lateral')
-  txb = st.sidebar.text_input('Escriba algo en la barra:')
-  st.sidebar.write(f'Escribiste en la barra: {txb}')
