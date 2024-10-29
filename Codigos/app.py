1b0deaede4877b3bab04caa1e223a07364556b86
K4m1K4z3 <sebastian141718floresflores@gmail.com>
2024-10-29 15:27:22 -0300

    Delete Codigos/app.py

--- Codigos/app.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-if st.button("Home"):
-    st.sidebar.switch_page("app.py")
-if st.button("Page 1"):
-    st.sidebar.switch_page("Archivos/Clase_1.py")
-if st.button("Page 2"):
-    st.sidebar.switch_page("Archivos/Clase_2.py")
-  
