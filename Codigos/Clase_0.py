ee5eabc53d09eb971be34c84ed5e7b6df3d93b93
K4m1K4z3 <sebastian141718floresflores@gmail.com>
2024-10-30 06:52:26 -0300

    Actualizar Clase_0.py

--- Codigos/Clase_0.py
+++ Codigos/Clase_0.py
@@ -3,4 +3,5 @@ import streamlit as st
 def c0():
   st.header('Bienvenido a la pagina principal')
   st.subheader('Aqui estara el posible subtitulo de mi pagina principal')
+  st.write('El ADN humano es 50% idéntico al de un plátano.')
   
